7a72b334debbad7d5fdd0428622d071321709684
Werner Kramer <werner.kramer@vortech.nl>
2018-12-05 12:26:12 +0100

    Add chartArea

---
+++ src/Charts/chartArea.ts
@@ -0,0 +1,64 @@
+import * as d3 from 'd3'
+import { CartesianAxis, PolarAxis } from '../Axis'
+import { Chart, AUTO_SCALE } from './charts'
+
+function mean(x: number[] | number) {
+  if (x instanceof Array) {
+    return d3.mean(x)
+  }
+  return x
+}
+
+export class ChartArea extends Chart {
+  plotterCartesian(axis: CartesianAxis, dataKeys: any) {
+    let xkey = dataKeys.xkey ? dataKeys.xkey : 'x'
+    let ykey = dataKeys.ykey ? dataKeys.ykey : 'y'
+    let colorkey = dataKeys.colorkey
+
+    let colorScale = d3.scaleLinear().domain([0, 1])
+    if (this.options.colorScale === AUTO_SCALE) {
+      colorScale.domain(
+        d3.extent(this.data, function(d: any): number {
+          return d[colorkey]
+        })
+      )
+    }
+
+    let colorMap = this.colorMap
+
+    let mappedData: any = this.data.map(function(d: any) {
+      return {
+        x: d[xkey],
+        y: d[ykey].map(axis.yScale),
+        color: colorMap(colorScale(mean(d[colorkey])))
+      }
+    })
+
+    this.group = this.selectGroup(axis, 'chart-area').append('path')
+
+    let areaGenerator = d3
+      .area()
+      .x(function(d: any) {
+        return axis.xScale(d.x)
+      })
+      .y0(function(d: any) {
+        return d.y[0]
+      })
+      .y1(function(d: any) {
+        return d.y[1]
+      })
+
+    let elements = this.group.datum(mappedData)
+
+    let t = d3
+      .transition()
+      .duration(this.options.transitionTime)
+      .ease(d3.easeLinear)
+
+    elements.attr('d', areaGenerator)
+  }
+
+  plotterPolar(axis: PolarAxis, dataKeys: any) {
+    console.error('plotterPolar is not implemented for ChartArea')
+  }
+}
--- src/Charts/index.ts
+++ src/Charts/index.ts
@@ -1,4 +1,5 @@
 export * from './charts'
+export * from './chartArea'
 export * from './chartMarker'
 export * from './chartLine'
 export * from './chartRange'
