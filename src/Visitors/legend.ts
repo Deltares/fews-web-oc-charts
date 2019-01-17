73c07e87e7605cc5c3a31e6e11cb752d7d6b5483
Werner Kramer <werner.kramer@vortech.nl>
2019-01-17 07:35:31 +0100

    Add loading spinner to legend

--- src/Visitors/legend.ts
+++ src/Visitors/legend.ts
@@ -1,5 +1,5 @@
 import * as d3 from 'd3'
-import { Axis } from '../Axis'
+import { Axis, CartesianAxis } from '../Axis'
 import { Visitor } from './visitor'
 import { ChartLine, ChartArea } from '../Charts'
 
@@ -8,6 +8,7 @@ export class Legend implements Visitor {
   private labels: any
   private svg: any
   private group: any
+  private axis: CartesianAxis
 
   constructor(labels: any, container?: HTMLElement) {
     this.container = container
@@ -19,22 +20,30 @@ export class Legend implements Visitor {
   }
 
   visit(axis: Axis) {
+    this.axis = axis as CartesianAxis
     this.createLegend(axis)
   }
 
   createLegend(axis: Axis) {
-    this.svg.attr('width', axis.margin.left + axis.width + axis.margin.right).attr('height', 20)
-    if (this.group == null) this.group = this.svg.append('g')
-    this.group.attr('transform', 'translate(' + axis.margin.left + ', 0)')
-    let dx = Math.round(axis.width / Object.keys(this.labels).length)
+    this.svg.attr('width', axis.margin.left + axis.width + axis.margin.right).attr('height', 100)
+    if (!this.group) this.group = this.svg.append('g')
+    this.redraw()
+  }
+
+  redraw() {
+    this.group.attr('transform', 'translate(' + this.axis.margin.left + ', 0)')
+    let dx = Math.round(this.axis.width / Object.keys(this.labels).length)
+    this.group.selectAll('g').remove()
     let entries = this.group.selectAll('g').data(this.labels)
+    let that = this
     let enter = entries
       .enter()
       .append('g')
+      .attr('class', 'legend-entry')
+      .merge(entries)
       .attr('transform', function(d, i) {
         return 'translate(' + i * dx + ',10)'
       })
-      .attr('class', 'legend-entry')
       .each(function(d, i) {
         console.log(d, i)
         let entry = d3.select(this)
@@ -42,26 +51,45 @@ export class Legend implements Visitor {
           .select(d.selector)
           .select('path')
           .node() as Element
-        let style = window.getComputedStyle(chartElement)
-        let chart = axis.charts.filter(x => x.id === d.selector)
-        if (chart[0] instanceof ChartLine) {
+        if (chartElement) {
+          let style = window.getComputedStyle(chartElement)
+          let chart = that.axis.charts.filter(x => x.id === d.selector)
+          if (chart[0] instanceof ChartLine) {
+            entry
+              .append('line')
+              .attr('x1', 0)
+              .attr('x2', 20)
+              .attr('y1', 0)
+              .attr('y2', 0)
+              .style('stroke', style.getPropertyValue('stroke'))
+              .style('stroke-width', style.getPropertyValue('stroke-width'))
+              .style('stroke-dasharray', style.getPropertyValue('stroke-dasharray'))
+          } else if (chart[0] instanceof ChartArea) {
+            entry
+              .append('rect')
+              .attr('x', 0)
+              .attr('y', -5)
+              .attr('width', 20)
+              .attr('height', 10)
+              .style('fill', style.getPropertyValue('fill'))
+          }
+          entry.on('click', function() {
+            let display = style.getPropertyValue('visibility')
+            if (display === 'visible') {
+              d3.selectAll(d.selector).style('visibility', 'hidden')
+              entry.style('opacity', 0.5)
+            } else {
+              d3.selectAll(d.selector).style('visibility', 'visible')
+              entry.style('opacity', 1.0)
+            }
+          })
+        } else {
           entry
-            .append('line')
-            .attr('x1', 0)
-            .attr('x2', 20)
-            .attr('y1', 0)
-            .attr('y2', 0)
-            .style('stroke', style.getPropertyValue('stroke'))
-            .style('stroke-width', style.getPropertyValue('stroke-width'))
-            .style('stroke-dasharray', style.getPropertyValue('stroke-dasharray'))
-        } else if (chart[0] instanceof ChartArea) {
-          entry
-            .append('rect')
-            .attr('x', 0)
-            .attr('y', -4)
-            .attr('width', 20)
-            .attr('height', 10)
-            .style('fill', style.getPropertyValue('fill'))
+            .append('circle')
+            .attr('class', 'spinner')
+            .attr('cx', 10)
+            .attr('cy', 0)
+            .attr('r', 8)
         }
         entry
           .append('text')
@@ -69,18 +97,8 @@ export class Legend implements Visitor {
           .attr('x', 25)
           .attr('y', 0)
           .style('dominant-baseline', 'middle')
-        entry.on('click', function() {
-          let display = style.getPropertyValue('visibility')
-          if (display === 'visible') {
-            d3.selectAll(d.selector).style('visibility', 'hidden')
-            entry.style('opacity', 0.5)
-          } else {
-            d3.selectAll(d.selector).style('visibility', 'visible')
-            entry.style('opacity', 1.0)
-          }
-        })
       })
-    //update
+    // update
     entries.attr('transform', function(d, i) {
       return 'translate(' + i * dx + ',10)'
     })
