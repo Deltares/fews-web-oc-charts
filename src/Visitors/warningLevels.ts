8a439138ffb025f450371cb93b444d8ccc2687ee
Werner Kramer <werner.kramer@vortech.nl>
2019-01-17 11:29:51 +0100

    Fix resize for warningLevels

--- src/Visitors/warningLevels.ts
+++ src/Visitors/warningLevels.ts
@@ -4,6 +4,11 @@ import { Visitor } from './visitor'
 
 export class WarningLevels implements Visitor {
   private escalationLevels: any[]
+  private group: any
+  private axis: CartesianAxis
+  private scale: any
+  private warningAxis: any
+  private sections: any
 
   constructor() {
     this.escalationLevels = [
@@ -15,26 +20,38 @@ export class WarningLevels implements Visitor {
   }
 
   visit(axis: Axis) {
+    this.axis = axis as CartesianAxis
     this.create(axis as CartesianAxis)
   }
 
   create(axis: CartesianAxis) {
-    let escalationsScale = d3
-      .scaleLinear()
-      .domain(axis.yScale.domain())
-      .range(axis.yScale.range())
-    let escalations = axis.canvas
-      .append('g')
-      .attr('transform', 'translate(' + axis.width + ' ,0)')
-      .attr('class', 'axis y2-axis')
+    if (!this.scale) {
+      this.scale = d3.scaleLinear()
+    }
+    this.scale.domain(axis.yScale.domain()).range(axis.yScale.range())
+
+    if (!this.warningAxis) {
+      this.warningAxis = d3
+        .axisRight(this.scale)
+        .tickValues([-100, 100, 110, 140])
+        .tickFormat(function(d, i) {
+          return escalationLevels[i].id
+        })
+    }
+
+    if (!this.group) {
+      this.group = d3
+      axis.canvas
+        .append('g')
+        .attr('class', 'axis y2-axis')
+        .attr('transform', 'translate(' + axis.width + ' ,0)')
+    }
+
+    this.group.select('.y2-axis').attr('transform', 'translate(' + axis.width + ' ,0)')
+
     let escalationLevels = this.escalationLevels
-    let escalationsAxis = d3
-      .axisRight(escalationsScale)
-      .tickValues([-100, 100, 110, 140])
-      .tickFormat(function(d, i) {
-        return escalationLevels[i].id
-      })
-    let axisHandle = axis.canvas.select('.y2-axis').call(escalationsAxis)
+
+    let axisHandle = axis.canvas.select('.y2-axis').call(this.warningAxis)
     axisHandle
       .selectAll('.tick text')
       .append('title')
@@ -43,40 +60,51 @@ export class WarningLevels implements Visitor {
         return 'waarschuwing waardes' + escalationLevels[i].c + '' + d
       })
 
-    let sections = axis.canvas
-      .select('.axis-canvas')
-      .append('g')
-      .selectAll('rect')
-      .data(escalationsAxis.tickValues())
+    if (!this.sections) {
+      this.sections = this.axis.canvas
+        .select('.axis-canvas')
+        .append('g')
+        .attr('class', 'warning-sections')
+      this.sections
+        .selectAll('rect')
+        .data(this.warningAxis.tickValues())
+        .enter()
+        .append('rect')
+    }
 
+    this.redraw()
+  }
+
+  redraw() {
+    let escalationLevels = this.escalationLevels
+
+    let that = this
     let escY = function(d, i) {
       if (escalationLevels[i].c === '<') {
-        return escalationsScale(d)
+        return that.scale(d)
       } else {
         if (i === escalationLevels.length - 1) return 0
-        return escalationsScale(escalationLevels[i + 1].val)
+        return that.scale(escalationLevels[i + 1].val)
       }
     }
 
     let escHeight = function(d, i) {
       if (escalationLevels[i].c === '<') {
-        if (i === 0) return axis.height - escalationsScale(d)
-        return escalationsScale(escalationLevels[i - 1].val) - escalationsScale(d)
+        if (i === 0) return that.axis.height - that.scale(d)
+        return that.scale(escalationLevels[i - 1].val) - that.scale(d)
       } else {
-        if (i === escalationLevels.length - 1) return escalationsScale(d)
-        return escalationsScale(d) - escalationsScale(escalationLevels[i + 1].val)
+        if (i === escalationLevels.length - 1) return that.scale(d)
+        return that.scale(d) - that.scale(escalationLevels[i + 1].val)
       }
     }
     let escFill = function(d, i) {
       return escalationLevels[i].color
     }
 
-    sections
-      .enter()
-      .append('rect')
-      .merge(sections)
+    this.sections
+      .selectAll('rect')
       .attr('y', escY)
-      .attr('width', axis.width)
+      .attr('width', this.axis.width)
       .attr('height', escHeight)
       .attr('fill', escFill)
   }
