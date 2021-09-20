e126a1e46f00ffa276816713fb8c129023b5e0d4
Werner Kramer <werner.kramer@vortech.nl>
2021-09-20 20:33:57 +0000

    Resolve "Fix warningLevel labels"

--- src/Visitors/warningLevels.ts
+++ src/Visitors/warningLevels.ts
@@ -8,9 +8,8 @@ export interface WarningLevelOptions {
   };
 }
 
-
 export class WarningLevels implements Visitor {
-  private escalationLevels: any[]
+  public escalationLevels: any[]
   private group: any
   private axis: CartesianAxis
   private scale: any
@@ -30,19 +29,19 @@ export class WarningLevels implements Visitor {
     }
   }
 
-  visit(axis: Axis) {
+  visit(axis: Axis): void {
     this.axis = axis as CartesianAxis
     this.create(axis as CartesianAxis)
   }
 
-  create(axis: CartesianAxis) {
-    let scale = (this.scale = d3.scaleLinear())
+  create(axis: CartesianAxis): void {
+    const scale = (this.scale = d3.scaleLinear())
     this.scale.domain(axis.yScale[this.options.y.axisIndex].domain()).range(axis.yScale[this.options.y.axisIndex].range())
-    let escalationLevels = this.escalationLevels
+    const escalationLevels = this.escalationLevels
 
-    let tickValues = escalationLevels
+    const tickValues = escalationLevels
       .filter(function(el) {
-        let domain = scale.domain()
+        const domain = scale.domain()
         return el.val >= domain[0] && el.val <= domain[1]
       })
       .map(function(el) {
@@ -66,7 +65,7 @@ export class WarningLevels implements Visitor {
       .attr('class', 'axis y2-axis')
       .attr('transform', 'translate(' + axis.width + ' ,0)')
 
-    let axisHandle = axis.canvas.select('.y2-axis').call(this.warningAxis)
+    const axisHandle = axis.canvas.select('.y2-axis').call(this.warningAxis)
     axisHandle
       .selectAll('.tick text')
       .append('title')
@@ -83,30 +82,37 @@ export class WarningLevels implements Visitor {
     this.redraw()
   }
 
-  redraw() {
-    let escalationLevels = this.escalationLevels
-    let scale = this.axis.yScale[0].copy()
-    let tickValues = escalationLevels
+  redraw(): void {
+    const escalationLevels = this.escalationLevels
+    const scale = this.axis.yScale[0].copy()
+    const tickValues = escalationLevels
       .filter(function(el) {
-        let domain = scale.domain()
+        const domain = scale.domain()
         return el.val >= domain[0] && el.val <= domain[1]
       })
       .map(function(el) {
         return el.val
       })
 
-    this.warningAxis.tickValues(tickValues)
-    this.warningAxis.scale(scale)
+    this.warningAxis
+      .scale(scale)
+      .tickValues(tickValues)
+      .tickFormat(function (d, i) {
+        let level
+        for (level of escalationLevels) {
+          if (level.val === d) break
+        }
+        return level.id
+      })
 
-    let transition = d3.transition().duration(this.transitionTime)
+    const transition = d3.transition().duration(this.transitionTime)
     this.group
       .select('.y2-axis')
       .attr('transform', 'translate(' + this.axis.width + ' ,0)')
       .transition(transition)
       .call(this.warningAxis)
 
-    let that = this
-    let escY = function(d, i) {
+    const escY = (d, i) => {
       if (d.c === '<') {
         return scale(d.val)
       } else {
@@ -115,9 +121,9 @@ export class WarningLevels implements Visitor {
       }
     }
 
-    let escHeight = function(d, i) {
+    const escHeight = (d, i) => {
       if (d.c === '<') {
-        if (i === 0) return Math.max(0, that.axis.height - scale(d.val))
+        if (i === 0) return Math.max(0, this.axis.height - scale(d.val))
         return Math.max(0, scale(escalationLevels[i - 1].val) - scale(d.val))
       } else {
         if (i === escalationLevels.length - 1) return Math.max(0, scale(d.val))
@@ -125,7 +131,7 @@ export class WarningLevels implements Visitor {
       }
     }
 
-    let rects = this.sections
+    const rects = this.sections
       .selectAll('rect')
       .data(this.escalationLevels)
 
