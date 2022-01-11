ea3675bdda2c323ecf688747d8e070cf0a071f48
Werner Kramer <werner.kramer@vortech.nl>
2022-01-11 14:13:33 +0100

    Clean up code

--- src/Utils/date.ts
+++ src/Utils/date.ts
@@ -12,20 +12,24 @@ export function generateMultiFormat(timeZone: string | Zone, locale?: string) {
   return function (date: Date) {
     const m = DateTime.fromJSDate(date).setZone(timeZone).setLocale(locale ?? navigator.language)
     const offsetDate = new Date(date.getTime() + m.offset * 60000)
-    return (d3.utcSecond(offsetDate) < offsetDate
-      ? m.toFormat('.SSS')
-      : d3.utcMinute(offsetDate) < offsetDate
-        ? m.toFormat(':ss')
-        : d3.utcHour(offsetDate) < offsetDate
-          ? m.toFormat('HH:mm')
-          : d3.utcDay(offsetDate) < offsetDate
-            ? m.toFormat('HH:mm')
-            : d3.utcMonth(offsetDate) < offsetDate
-              ? d3.utcWeek(offsetDate) < offsetDate
-                ? m.toFormat('EEE dd')
-                : m.toFormat('MMM dd')
-              : d3.utcYear(offsetDate) < offsetDate
-                ? m.toFormat('MMMM')
-                : m.toFormat('yyyy'))
+    let formatString: string
+    if (d3.utcSecond(offsetDate) < offsetDate) {
+      formatString = '.SSS'
+    } else if (d3.utcMinute(offsetDate) < offsetDate) {
+      formatString = ':ss'
+    } else if (d3.utcHour(offsetDate) < offsetDate) {
+      formatString = 'HH:mm'
+    } else if (d3.utcDay(offsetDate) < offsetDate) {
+      formatString = 'HH:mm'
+    } else if (d3.utcMonth(offsetDate) < offsetDate) {
+      formatString = d3.utcWeek(offsetDate) < offsetDate
+        ? m.toFormat('EEE dd')
+        : m.toFormat('MMM dd')
+    } else {
+      formatString = d3.utcYear(offsetDate) < offsetDate
+        ? m.toFormat('MMMM')
+        : m.toFormat('yyyy')
+      }
+    return m.toFormat(formatString)
   }
 }
