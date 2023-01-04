579fefadd218a50ad031a8e16f43232b2bdbcbe0
Werner Kramer <werner.kramer@vortech.nl>
2023-01-04 10:50:50 +0100

    Fix AtZero regression

--- src/Axis/xAxis.ts
+++ src/Axis/xAxis.ts
@@ -9,10 +9,8 @@ export interface GridOptions {
 export class XAxis extends Axis {
 
   translateAxis(position): string {
-    console.log(position, AxisPosition.Bottom)
-    console.log(position === AxisPosition.Bottom)
     if (position === AxisPosition.AtZero) {
-      return `translate(0,${this.spanScale.scale(0)})`
+      return `translate(0,${this.spanScale(0)})`
     } else if (position === AxisPosition.Bottom) {
       return `translate(0,${this.spanScale.range()[0]})`
     }
--- src/Axis/yAxis.ts
+++ src/Axis/yAxis.ts
@@ -5,7 +5,7 @@ export class YAxis extends Axis {
 
   translateAxis(position): string {
     if (position === AxisPosition.AtZero) {
-      return `translate(${this.spanScale.scale(0)},0)`
+      return `translate(${this.spanScale(0)},0)`
     } else if (position === AxisPosition.Right) {
       return `translate(${this.spanScale.range()[1]},0)`
     }
