6780df632feeff1ce70c5e1278496139fef13f8c
Martina Pippi <martina.pippi@vortech.nl>
2023-05-12 10:27:28 +0200

    added unit tests AlertLines

---
+++ test/testAlertLines.spec.ts
@@ -0,0 +1,13 @@
+// unit tests for the testAlertLines.ts file
+import {AlertLines} from '../src/Visitors/alertLines'
+
+describe('test for AlertLines', () => {
+    it('should create an alert line', () => {
+        const alertLines = new AlertLines([{x1: 0, x2: 1, value: 0, color: 'red', yAxisIndex: 0}])
+        expect(alertLines.options.length).toBe(1)
+    })
+    it('should not create an alert line', () => {
+        const alertLines = new AlertLines([])
+        expect(alertLines.options.length).toBe(0)  
+    })
+})
