b31f6f9b1ab432260dba4f401a5a181fa2d4f0a9
Cees Voesenek <cees.voesenek@vortech.nl>
2023-07-18 17:15:23 +0200

    Add documentation to Axes

--- src/Axes/axes.ts
+++ src/Axes/axes.ts
@@ -32,6 +32,18 @@ export interface AxisIndex {
   color?: { key: string };
 }
 
+/**
+ * Base class representing a set of axes (i.e., a domain) in which data are visualised.
+ * 
+ * It requires a specific implementation for different domain types, e.g. Cartesian, polar.
+ * 
+ * This creates a SVG root element into which the chart is drawn.
+ * 
+ * Note that this represents the domain in which we are drawing, and not the visualisation of the
+ * axes with lines, ticks, etc---this is represented by the Axis class.
+ * 
+ * @see Axis
+ */
 export abstract class Axes {
   container: HTMLElement
   svg: D3Selection<SVGSVGElement>
@@ -51,6 +63,15 @@ export abstract class Axes {
   charts: Chart[]
   visitors: Visitor[]
 
+  /**
+   * Initialises an Axes object.
+   * 
+   * @param container HTML element to which the SVG element will be appended.
+   * @param width Width of the container in which the root SVG element will be created.
+   * @param height Height of the container in which the root SVG element will be created.
+   * @param options Options for the axes.
+   * @param defaultOptions Default options for the axes.
+   */
   constructor(container: HTMLElement, width: number | null, height: number | null, options: AxesOptions, defaultOptions: any) {
     this.container = container
     this.options = defaultsDeep(
@@ -64,7 +85,6 @@ export abstract class Axes {
     })
     this.observer.observe(container)
 
-    // Using the d3.formatLocale is not easy for generic plotting
     d3.formatDefaultLocale({
       decimal: '.',
       thousands: '\u2009',
@@ -73,6 +93,8 @@ export abstract class Axes {
     })
 
     this.margin = { ...{ top: 40, right: 40, bottom: 40, left: 40 }, ...options.margin }
+
+    // Create the root SVG element.
     this.svg = d3
       .select(container)
       .append('svg')
@@ -80,14 +102,31 @@ export abstract class Axes {
       .attr('overflow', 'visible')
     this.setSize(height, width)
 
+    // Create an empty <defs> element and a <g> element that we will draw into (i.e. our "canvas").
     this.defs = this.svg.append('defs')
     this.canvas = this.svg
       .append('g')
       .attr('transform', 'translate(' + this.margin.left + ',' + this.margin.top + ')')
+
     this.tooltip = new Tooltip(container)
     this.charts = []
     this.visitors = []
   }
+
+  /**
+   * Sets the ranges for each axis.
+   */
+  protected abstract setRange(): void
+
+  /**
+   * Reset axes to its default state, then redraw all elements.
+   */
+  abstract redraw(): void
+
+  /**
+   * Draw all elements in the axes.
+   */
+  abstract update(): void
 
   setDefaultTimeOptions(axisOptions: AxisOptions | AxisOptions[]) {
     const optionsArray = Array.isArray(axisOptions) ? axisOptions : [axisOptions]
@@ -97,21 +136,39 @@ export abstract class Axes {
     }
   }
 
-  setOptions(options: AxesOptions): void {
+  /**
+   * Sets options for these axes.
+   * 
+   * Any options not specified in the argument will be left at the value they had previously.
+   * 
+   * @param options Options to set for the current axes.
+   */
+  setOptions(options: Partial<AxesOptions>): void {
     merge(this.options,
       options
     )
   }
 
+  /**
+   * Sets the width and height of the root SVG element based and our "canvas".
+   * 
+   * @param width Width of the container in which the root SVG element will be created.
+   * @param height Height of the container in which the root SVG element will be created.
+   */
   setSize(height?: number, width?: number): void {
-    const containerWidth = width == null ? this.container.offsetWidth : width
-    const containerHeight = height == null ? this.container.offsetHeight : height
+    const containerWidth = width ?? this.container.offsetWidth
+    const containerHeight = height ?? this.container.offsetHeight
+
+    // The "width" and "height" properties are the width and height of the "canvas" in which we
+    // are drawing, which takes into account the specified margins.
     this.height = containerHeight - this.margin.top - this.margin.bottom
     this.width = containerWidth - this.margin.left - this.margin.right
     if (this.height < 0 || this.width < 0) {
       this.height = 0
       this.width = 0
     }
+
+    // The root SVG element is set to occupy the container size.
     this.svg
       .attr('width', containerWidth)
       .attr('height', containerHeight)
@@ -119,20 +176,27 @@ export abstract class Axes {
 
   }
 
+  /**
+   * Resize the Axes, for example when the container size changes.
+   */
   resize(): void {
     this.setSize()
     this.setRange()
     this.redraw()
   }
 
-  abstract redraw(): void
-
+  /**
+   * Resets the zoom level to its initial value.
+   */
   resetZoom(): void {
     this.redraw()
   }
 
-  abstract update(): void
-
+  /**
+   * Removes the specified chart from the axes.
+   * 
+   * @param id Identifier of the chart to remove.
+   */
   removeChart(id: string): void {
     let i: number
     for (i = 0; i < this.charts.length; i++) {
@@ -145,6 +209,9 @@ export abstract class Axes {
     this.chartGroup.selectAll(`[data-chart-id="${id}"]`).remove()
   }
 
+  /**
+   * Removes all charts from the axes.
+   */
   removeAllCharts(): void {
     for (const chart of this.charts) {
       chart.group = null
@@ -153,6 +220,11 @@ export abstract class Axes {
     this.chartGroup.selectAll('g').remove()
   }
 
+  /**
+   * Adds the specified visitor to this axes' list of visitors.
+   * 
+   * @param v Visitor to accept.
+   */
   accept(v: Visitor): void {
     this.visitors.push(v)
     v.visit(this)
@@ -173,9 +245,11 @@ export abstract class Axes {
     return _extent
   }
 
+  /**
+   * Creates an empty group (i.e. <g> element) to add charts to.
+   */
   createChartGroup(): void {
     this.chartGroup = this.canvas.append('g').attr('class', 'charts')
   }
 
-  protected abstract setRange(): void
 }
