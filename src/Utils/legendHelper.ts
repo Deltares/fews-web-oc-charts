70fddffb440e90d90c922e61d654296572211302
Werner Kramer <werner.kramer@vortech.nl>
2023-11-03 11:21:57 +0100

    Fix typo toggleChartVisibility

--- src/Utils/legendHelper.ts
+++ src/Utils/legendHelper.ts
@@ -1,6 +1,6 @@
 import { Axes } from '../Axes/axes.js'
 
-export function toggleChartVisisbility(axis: Axes, id: string): void {
+export function toggleChartVisibility(axis: Axes, id: string): void {
   for (const chart of axis.charts.filter(c => c.id === id)) {
     chart.visible = !chart.visible
   }
