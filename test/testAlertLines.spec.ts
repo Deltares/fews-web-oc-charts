7533be89aaf3ad7367bce68562f015628c033f04
Werner Kramer <werner.kramer@vortech.nl>
2023-11-03 11:34:55 +0100

    Fix altert line test

--- test/testAlertLines.spec.ts
+++ test/testAlertLines.spec.ts
@@ -3,7 +3,7 @@ import {AlertLines} from '../src/Visitors/alertLines'
 
 describe('test for AlertLines', () => {
     it('should create an alert line', () => {
-        const alertLines = new AlertLines([{x1: 0, x2: 1, value: 0, color: 'red', yAxisIndex: 0}])
+        const alertLines = new AlertLines([{x1: 0, x2: 1, value: 0, color: 'red', yAxisIndex: 0, description: 'Test alert line'}])
         expect(alertLines.options.length).toBe(1)
     })
     it('should not create an alert line', () => {
