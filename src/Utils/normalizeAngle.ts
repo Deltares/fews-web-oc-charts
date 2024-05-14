f2e21a737d2e7f654b256c42b65ac857d3ba1731
Werner Kramer <werner.kramer@vortech.nl>
2024-05-14 17:27:54 +0200

    Add function doc

--- src/Utils/normalizeAngle.ts
+++ src/Utils/normalizeAngle.ts
@@ -1,3 +1,8 @@
+/**
+ * Normalizes an angle to be within the range of 0 to 360 degrees.
+ * @param angle - The angle to normalize.
+ * @returns The normalized angle.
+ */
 export function normalizeAngle(angle) {
   return angle - 360 * Math.floor((angle) / 360)
 }
