46467d28379383e60166de25d4eb7c630204997c
Werner Kramer <werner.kramer@vortech.nl>
2024-12-16 10:24:23 +0100

    Add filter option to dataExtent function

--- test/Data/dataExtentFor.spec.ts
+++ test/Data/dataExtentFor.spec.ts
@@ -1,13 +1,26 @@
 import { dataExtentFor } from '../../src/Data/dataExtentFor'
 
-/**
- * Dummy test
- */
 describe('dataExtentFor', () => {
   it('scalar x valued data', () => {
     const data = [
       {
         x: 0,
+      },
+      {
+        x: -1,
+      },
+      {
+        x: 1,
+      },
+    ]
+    const extent = dataExtentFor(data, 'x')
+    expect(extent).toStrictEqual([-1, 1])
+  })
+
+  it('scalar x valued data with NaN', () => {
+    const data = [
+      {
+        x: NaN,
       },
       {
         x: -1,
@@ -56,3 +69,92 @@ describe('dataExtentFor', () => {
   })
 
 })
+
+describe('dataExtentFor with filter', () => {
+  it('unfiltered x valued data', () => {
+    const data = [
+      {
+        x: 999,
+      },
+      {
+        x: -1,
+      },
+      {
+        x: 1,
+      },
+    ]
+    const extent = dataExtentFor(data, 'x')
+    expect(extent).toStrictEqual([-1, 999])
+  })
+
+
+  it('fiterscalar x valued data', () => {
+    const data = [
+      {
+        x: 999,
+      },
+      {
+        x: -1,
+      },
+      {
+        x: 1,
+      },
+    ]
+    const filter = (d) => d.x !== 999
+    const extent = dataExtentFor(data, 'x', filter)
+    expect(extent).toStrictEqual([-1, 1])
+  })
+
+
+  it('scalar x valued data', () => {
+    const data = [
+      {
+        x: 999, flag: 4
+      },
+      {
+        x: -1,
+      },
+      {
+        x: 1,
+      },
+    ]
+    const filter = (d: {flag?: number}) => d.flag !== 4
+    const extent = dataExtentFor(data, 'x', filter)
+    expect(extent).toStrictEqual([-1, 1])
+  })
+
+  it('all data filtered', () => {
+    const data = [
+      {
+        x: 999,
+      },
+      {
+        x: 999,
+      },
+      {
+        x: 999,
+      },
+    ]
+    const filter = (d) => d.x !== 999
+    const extent = dataExtentFor(data, 'x', filter)
+    expect(extent).toStrictEqual([undefined, undefined])
+  })
+
+  it('all flagged data', () => {
+    const data = [
+      {
+        x: 999, flag: 4
+      },
+      {
+        x: -1, flag: 4
+      },
+      {
+        x: 1, flag: 4
+      },
+    ]
+    const filter = (d) => d.flag !== 4
+    const extent = dataExtentFor(data, 'x', filter)
+    expect(extent).toStrictEqual([undefined, undefined])
+  })
+})
+
