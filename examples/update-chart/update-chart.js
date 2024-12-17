a86ca6ecf16ba492cbf3510698f883540ee5f0e7
Anne Markensteijn <anne.markensteijn@vortech.nl>
2024-12-17 14:38:35 +0100

    Add example to update data
    Start with very simple chart with fixed data

---
+++ examples/update-chart/update-chart.js
@@ -0,0 +1,65 @@
+function onLoad() {
+  var currentTime = new Date()
+  var timeStep = 5 * 60 * 60 * 1000 // 5 minutes
+  var windDirectionData = [
+    {
+      t: new Date(currentTime.getTime() - timeStep),
+      y: 140
+    },
+    {
+      t: currentTime,
+      y: 170
+    },
+    {
+      t: new Date(currentTime.getTime() + timeStep),
+      y: 150
+    },
+  ]
+
+  var axisOptions = {
+    x: [
+      {
+        type: 'time',
+        position: wbCharts.AxisPosition.Bottom,
+        showGrid: true,
+      },
+    ],
+    y: [
+      {
+        label: 'windrichting',
+        unit: '°',
+        type: 'degrees',
+        domain: [0, 360],
+        position: wbCharts.AxisPosition.Left,
+        showGrid: true,
+      },
+    ],
+    margin: {
+      left: 50,
+      right: 50,
+    },
+  }
+  var chartContainer = document.getElementById('chart-wind-direction-time')
+  var axis = new wbCharts.CartesianAxes(chartContainer, null, null, axisOptions)
+  var windDirectionModel = new wbCharts.ChartLine(windDirectionData, {})
+  windDirectionModel.addTo(axis, { x: { key: 't', axisIndex: 0 }, y: { key: 'y', axisIndex: 0 } },
+    'winddirection',
+    '#winddirection-line-time'
+  )
+
+  var legendWindDirection = new wbCharts.Legend(
+    [
+      { selector: 'winddirection', label: 'Windrichting meting' },
+    ],
+    document.getElementById('legend-wind-direction-time')
+  )
+
+  var mouseOver = new wbCharts.MouseOver(['winddirection'])
+
+  axis.redraw({ x: { autoScale: true }, y: { autoScale: true } })
+
+  axis.accept(mouseOver)
+  axis.accept(legendWindDirection)
+}
+
+window.addEventListener('load', onLoad)
