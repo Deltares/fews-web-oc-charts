30d800b3f499be0a23c41f95dd7c0d1d1a0e2416
Cees Voesenek <cees.voesenek@vortech.nl>
2025-01-27 16:19:27 +0100

    Disable many any-related linting rules

--- eslint.config.js
+++ eslint.config.js
@@ -12,5 +12,25 @@ export default tseslint.config(
         },
       },
     },
+    rules: {
+      // Disable all rules related to "any", there's just too many any's to
+      // fix...
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unsafe-argument': 'off',
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-call': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+      '@typescript-eslint/no-unsafe-return': 'off',
+      '@typescript-eslint/no-unsafe-function-type': 'off',
+      // Allow unused variables if they are preceded by a _.
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
+    },
   }
 )
