ac82a3717ae14726514a71a4570d58cf9f7a14fa
Cees Voesenek <cees.voesenek@vortech.nl>
2025-02-11 09:32:05 +0100

    Simplify alert lines example

--- examples/alertLines/alertLines.ts
+++ examples/alertLines/alertLines.ts
@@ -2,142 +2,72 @@ import '@lib/scss/wb-charts.scss'
 import '@shared/shared.css'
 import './alertLines.css'
 
-import * as d3 from 'd3'
-import {
-  AlertLines,
-  AxisType,
-  CartesianAxes,
-  ChartArea,
-  ChartLine,
-  ChartMarker,
-  CurrentTime,
-  MouseOver,
-  ZoomHandler,
-} from '@lib'
-import { addListenerByClassName, percentile } from '@shared'
+import { AlertLines, AxisType, CartesianAxes, ChartLine } from '@lib'
+import { addListenerById, generateExampleData } from '@shared'
 
-const container = document.getElementById('chart-container-1')
-const axis = new CartesianAxes(container, null, null, {
-  x: [{ type: AxisType.time, showGrid: true }],
-  y: [
-    { label: 'Waterstand', unit: 'cm', showGrid: true, domain: [-450, 150] },
-    { label: 'Wind richting', unit: 'deg', position: 'right', domain: [-150, 450] },
-  ],
-  margin: { left: 50, right: 50 },
+addListenerById('theme-button', 'click', () => document.documentElement.classList.toggle('dark'))
+
+// Create new axes.
+const container = document.getElementById('chart-container')
+const axes = new CartesianAxes(container, null, null, {
+  x: [{ type: AxisType.time }],
+  y: [{ type: AxisType.value }],
+  automargin: true,
 })
 
-const refDate = new Date()
-const mouseOver = new MouseOver(['control', 'median'])
-const zoom = new ZoomHandler()
-const currentTime = new CurrentTime({ x: { axisIndex: 0 } })
-const alertLine = new AlertLines([])
+// Generate simple scalar example data.
+const startTime = new Date('2025-01-01T12:00Z')
+const endTime = new Date('2025-01-02T12:00Z')
+const exampleData = generateExampleData([startTime, endTime], [-2, 4], 100)
 
-interface InputData {
-  times: number[]
-  values: number[][]
-}
+// Create line.
+const line = new ChartLine(exampleData, {})
+line.addTo(axes, { x: { key: 'x', axisIndex: 0 }, y: { key: 'y', axisIndex: 0 } }, 'example', {
+  fill: 'none',
+  stroke: 'skyblue',
+  'stroke-width': '2',
+})
+axes.redraw({ x: { autoScale: true }, y: { autoScale: true } })
 
-function dataload() {
-  d3.json('../data/ensemble.json')
-    .then(function (data: InputData) {
-      const nEnsemble = data.values[0].length
-      const members = Array(nEnsemble)
-      const percentiles = [[], [], []]
-
-      for (let s = 0; s < nEnsemble; s++) {
-        members[s] = []
-      }
-      const medianTime = (data.times[0] + data.times[data.times.length - 1]) / 2
-
-      const x1 = new Date()
-      const x2 = new Date(x1.getTime() + 3 * 24 * 60 * 60 * 1000)
-
-      alertLine.options = [
-        {
-          x1,
-          x2,
-          value: 100,
-          yAxisIndex: 0,
-          description: 'warning',
-          color: '#00ffff',
-        },
-        {
-          x1,
-          x2,
-          value: -100,
-          yAxisIndex: 0,
-          description: 'alert',
-          color: '#FF0000',
-        },
-        {
-          x1,
-          x2,
-          value: 170,
-          yAxisIndex: 0,
-          description: 'out of bounds',
-          color: '#ffff00',
-        },
-      ]
-      data.times.forEach(function (time, index) {
-        const dateTime = new Date((time - medianTime) * 1000 + refDate.getTime())
-        for (let i = 0; i < nEnsemble; i++) {
-          members[i].push({ x: dateTime, y: data.values[index][i] })
-        }
-        const points = data.values[index]
-        percentiles[0].push({ x: dateTime, y: percentile(0.5, points) })
-        percentiles[1].push({ x: dateTime, y: percentile([0.25, 0.75], points) })
-        percentiles[2].push({ x: dateTime, y: percentile([0.05, 0.95], points) })
-      })
-
-      const plotControl1 = new ChartLine(members[0], {})
-      const plotMedian1 = new ChartMarker(percentiles[0], {})
-      const plotPercentile50 = new ChartArea(percentiles[1], {})
-      const plotPercentile90 = new ChartArea(percentiles[2], {})
-
-      const style90 = {
-        fill: 'skyblue',
-        stroke: 'skyblue',
-      }
-      const style50 = {
-        fill: 'deepskyblue',
-        stroke: 'deepskyblue',
-      }
-      plotPercentile90.addTo(
-        axis,
-        { x: { key: 'x', axisIndex: 0 }, y: { key: 'y', axisIndex: 0 } },
-        'percent90',
-        style90,
-      )
-      plotPercentile50.addTo(
-        axis,
-        { x: { key: 'x', axisIndex: 0 }, y: { key: 'y', axisIndex: 1 } },
-        'percent50',
-        style50,
-      )
-
-      plotControl1.addTo(
-        axis,
-        { x: { key: 'x', axisIndex: 0 }, y: { key: 'y', axisIndex: 0 } },
-        'control',
-        '#control-line',
-      )
-      plotMedian1.addTo(
-        axis,
-        { x: { key: 'x', axisIndex: 0 }, y: { key: 'y', axisIndex: 1 } },
-        'median',
-        '#median-line',
-      )
-      axis.accept(currentTime)
-      axis.accept(alertLine)
-      axis.redraw({ x: { autoScale: true }, y: { autoScale: true } })
-      axis.zoom()
-      axis.accept(zoom)
-      axis.accept(mouseOver)
-    })
-    .catch((error) => console.error(`Failed to create chart: ${error}`))
-}
-window.setTimeout(dataload, 1000)
-
-addListenerByClassName('theme-button', 'click', () =>
-  document.documentElement.classList.toggle('dark'),
-)
+// Create example alert lines.
+const alertLines = new AlertLines([
+  // Simple alert line that spans the exact date range.
+  {
+    x1: startTime,
+    x2: endTime,
+    value: 2,
+    description: 'Alert line inside range with full time range',
+    yAxisIndex: 0,
+    color: 'red',
+  },
+  // Alert line that spans a wider date range than the axis limits; the alert
+  // label will still be drawn within axis limits.
+  {
+    x1: new Date('2024-12-31T12:00Z'),
+    x2: new Date('2025-01-03T12:00Z'),
+    value: 1,
+    description: 'Alert line inside range with extended time range',
+    yAxisIndex: 0,
+    color: 'orange',
+  },
+  // Alert line that spans a narrower date range than the axis limits.
+  {
+    x1: new Date('2025-01-01T18:00Z'),
+    x2: new Date('2025-01-02T06:00Z'),
+    value: 0,
+    description: 'Alert line inside range with limited time range',
+    yAxisIndex: 0,
+    color: 'yellow',
+  },
+  // Alert line outside the y-range; the y-axis limits will not be automatically
+  // scaled to include the alert.
+  {
+    x1: startTime,
+    x2: endTime,
+    value: 5,
+    description: 'Alert line outside range',
+    yAxisIndex: 0,
+    color: 'green',
+  },
+])
+axes.accept(alertLines)
