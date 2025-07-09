54801e00c921ee4c6da7eb2b4fe38038bb26901e
hvangeffen <hunter.vangeffen@vortech.nl>
2025-07-09 15:05:45 +0200

    Send domain change event on scroll zoom

--- src/Visitors/zoomHandler.ts
+++ src/Visitors/zoomHandler.ts
@@ -196,7 +196,8 @@ export class ZoomHandler implements Visitor {
       if (isBandScale) continue
 
       const x = scale.invert(coord)
-      scale.domain([x - (x - scale.domain()[0]) * factor, x - (x - scale.domain()[1]) * factor])
+      const extent = [x - (x - scale.domain()[0]) * factor, x - (x - scale.domain()[1]) * factor]
+      axis.setDomain(axisKey, axisIndex, extent as [number, number] | [Date, Date])
     }
   }
 
