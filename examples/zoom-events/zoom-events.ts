c64dc1ab866342bd37e48d708b058b1bbfefd677
Cees Voesenek <cees.voesenek@vortech.nl>
2025-11-18 14:48:04 +0100

    Add event listeners for zoom and reset-zoom events

---
+++ examples/zoom-events/zoom-events.ts
@@ -0,0 +1,40 @@
+import '@lib/scss/wb-charts.scss'
+import '@shared/shared.css'
+import './zoom-events.css'
+
+import '@shared/theme-button'
+
+import { AxisType, CartesianAxes, ChartLine, WheelMode, ZoomHandler } from '@lib'
+import { generateExampleTimeSeriesData } from '@shared'
+
+function updateStatus(status: string): void {
+  const statusElement = document.getElementById('status')
+  statusElement.textContent = status
+}
+
+const exampleData = generateExampleTimeSeriesData(
+  [new Date('2025-01-01T00:00Z'), new Date('2025-01-05T00:00Z')],
+  [-1, 1],
+  100,
+)
+
+const container = document.getElementById('chart-container')
+const axes = new CartesianAxes(container, null, null, {
+  x: [{ type: AxisType.time }],
+  y: [{ type: AxisType.value, defaultDomain: [-1.5, 1.5] }],
+  automargin: true,
+})
+
+const line = new ChartLine(exampleData, {})
+line.addTo(axes, { x: { key: 'x', axisIndex: 0 }, y: { key: 'y', axisIndex: 0 } }, 'example', {
+  fill: 'none',
+  stroke: 'skyblue',
+  'stroke-width': '2',
+})
+axes.redraw({ x: { autoScale: true }, y: { autoScale: true } })
+
+const zoomHandler = new ZoomHandler(WheelMode.XY)
+axes.accept(zoomHandler)
+
+zoomHandler.addEventListener('zoom', () => updateStatus('Zoomed'))
+zoomHandler.addEventListener('reset-zoom', () => updateStatus('Zoom reset'))
