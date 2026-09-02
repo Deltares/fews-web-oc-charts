a262d2cde3eae4fd0c5e16bb598a7cd2dcd48c0f
Werner Kramer <werner.kramer@deltares.nl>
2026-09-02 13:52:34 +0200

    Cleanup setAlphaForColor

--- src/Utils/setAlphaForColor.ts
+++ src/Utils/setAlphaForColor.ts
@@ -1,20 +1,32 @@
+import { color } from 'd3'
+
 /**
  * Sets the alpha value for a given color supporting alpha channels.
  * @param color - The color to modify.
  * @param alpha - The alpha value to set (between 0 and 1).
  * @returns The modified color with the specified alpha value.
  */
-export function setAlphaForColor(color: string, alpha: number) {
-  if (color.startsWith('rgba') || color.startsWith('hsla')) {
-    return color.replace(/[\d.]+\)$/, `${alpha})`)
+export function setAlphaForColor(cssColor: string, alpha: number) {
+  const colorWithAlpha = color(cssColor)
+  if (cssColor.startsWith('rgba')) {
+    if (colorWithAlpha) {
+      colorWithAlpha.opacity = alpha
+      return colorWithAlpha.formatRgb()
+    }
   }
-  if (color.startsWith('#') && color.length === 9) {
+  if (cssColor.startsWith('hsla')) {
+    if (colorWithAlpha) {
+      colorWithAlpha.opacity = alpha
+      return colorWithAlpha.formatHsl()
+    }
+  }
+  if (cssColor.startsWith('#') && cssColor.length === 9) {
     return (
-      color.slice(0, -2) +
+      cssColor.slice(0, -2) +
       Math.round(255 * alpha)
         .toString(16)
         .toUpperCase()
     )
   }
-  return color
+  return cssColor
 }
