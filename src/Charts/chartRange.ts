1320dac2c18997e8c0564661dc4b73041b0cd463
Werner Kramer <werner.kramer@deltares.nl>
2026-09-16 23:23:58 +0200

    refactor: enhance type safety and improve data handling in ChartRange class

--- src/Charts/chartRange.ts
+++ src/Charts/chartRange.ts
@@ -1,21 +1,34 @@
 import * as d3 from 'd3'
 import { AxisIndex } from '../Axes/axes.js'
-import { CartesianAxes, PolarAxes } from '../index.js'
+import { CartesianAxes, CartesianAxesIndex, PolarAxes } from '../index.js'
 import { Chart, AUTO_SCALE } from './chart.js'
 import { TooltipAnchor, TooltipPosition } from '../Tooltip/tooltip.js'
 import type { DataPoint } from '../Data/types.js'
 
-function mean(x: number[] | number) {
+function mean(x: number[] | number): number {
   if (Array.isArray(x)) {
-    return d3.mean(x)
+    return d3.mean(x) ?? 0
   }
   return x
+}
+
+function numericRange(data: DataPoint, key: string): [number, number] {
+  const value = data[key]
+  if (
+    Array.isArray(value) &&
+    value.length >= 2 &&
+    typeof value[0] === 'number' &&
+    typeof value[1] === 'number'
+  ) {
+    return [value[0], value[1]]
+  }
+  throw new Error(`Expected ${key} to contain a numeric range`)
 }
 
 export class ChartRange extends Chart {
   private previousData: DataPoint[] = []
 
-  plotterCartesian(axis: CartesianAxes, axisIndex: AxisIndex) {
+  plotterCartesian(axis: CartesianAxes, axisIndex: CartesianAxesIndex) {
     const xKey = this.dataKeys.x
     const yKey = this.dataKeys.y
     const xScale = axis.xScales[axisIndex.x.axisIndex]
@@ -24,11 +37,13 @@ export class ChartRange extends Chart {
 
     const colorScale = d3.scaleLinear().domain([0, 1])
     if (this.options.colorScale === AUTO_SCALE) {
-      colorScale.domain(
-        d3.extent(this.data, (d: any): number => {
-          return d[colorKey]
-        }),
-      )
+      const colorValues = this.data
+        .map((d) => d[colorKey])
+        .filter((value): value is number => typeof value === 'number')
+      const colorExtent = d3.extent(colorValues)
+      if (colorExtent[0] !== undefined && colorExtent[1] !== undefined) {
+        colorScale.domain(colorExtent)
+      }
     }
 
     const colorMap = this.colorMap
@@ -36,7 +51,7 @@ export class ChartRange extends Chart {
     this.group = this.selectGroup(axis, 'chart-range')
     this.group.style('stroke', 'none')
 
-    const elements: any = this.group.selectAll('rect').data(this.data)
+    const elements = this.group.selectAll<SVGRectElement, DataPoint>('rect').data(this.data)
 
     const t = d3.transition().duration(this.options.transitionTime).ease(d3.easeLinear)
     // exit
@@ -45,24 +60,26 @@ export class ChartRange extends Chart {
     const update = elements
       .enter()
       .append('rect')
-      .attr('x', (d: any) => {
-        return xScale(d[xKey][0])
+      .attr('x', (d) => {
+        return xScale(numericRange(d, xKey)[0])
       })
-      .attr('y', (d: any) => {
-        return yScale(d[yKey][1])
+      .attr('y', (d) => {
+        return yScale(numericRange(d, yKey)[1])
       })
-      .attr('width', (d: any) => {
-        return xScale(d[xKey][1]) - xScale(d[xKey][0])
+      .attr('width', (d) => {
+        const range = numericRange(d, xKey)
+        return xScale(range[1]) - xScale(range[0])
       })
-      .attr('height', (d: any) => {
-        return yScale(d[yKey][0]) - yScale(d[yKey][1])
+      .attr('height', (d) => {
+        const range = numericRange(d, yKey)
+        return yScale(range[0]) - yScale(range[1])
       })
 
     if (this.options.tooltip !== undefined) {
       const tooltip = this.options.tooltip
 
       update
-        .on('pointerover', (_e: any, d) => {
+        .on('pointerover', (_e: Event, d: DataPoint) => {
           if (tooltip.anchor !== undefined && tooltip.anchor !== TooltipAnchor.Center) {
             console.error(
               'Tooltip not implemented for anchor ',
@@ -73,12 +90,17 @@ export class ChartRange extends Chart {
             )
           }
           axis.tooltip.show()
-          axis.tooltip.update(
-            this.toolTipFormatterCartesian(d),
-            this.options.tooltip?.position ?? TooltipPosition.Top,
-            axis.margin.left + (xScale(d[xKey][1]) + xScale(d[xKey][0])) / 2,
-            axis.margin.top + (yScale(d[yKey][1]) + yScale(d[yKey][0])) / 2,
-          )
+          const xRange = numericRange(d, xKey)
+          const yRange = numericRange(d, yKey)
+          const content = this.toolTipFormatterCartesian(d)
+          if (content !== undefined) {
+            axis.tooltip.update(
+              content,
+              tooltip.position ?? TooltipPosition.Top,
+              axis.margin.left + (xScale(xRange[1]) + xScale(xRange[0])) / 2,
+              axis.margin.top + (yScale(yRange[1]) + yScale(yRange[0])) / 2,
+            )
+          }
         })
         .on('pointerout', () => {
           axis.tooltip.hide()
@@ -86,24 +108,27 @@ export class ChartRange extends Chart {
     }
 
     if (colorKey) {
-      update.style('fill', (d: any) => {
-        return colorMap(colorScale(mean(d[colorKey])))
+      update.style('fill', (d) => {
+        const value = d[colorKey]
+        return typeof value === 'number' ? colorMap(colorScale(value)) : 'none'
       })
     }
 
     const enter = elements
       .transition(t)
-      .attr('x', (d: any) => {
-        return xScale(d[xKey][0])
+      .attr('x', (d) => {
+        return xScale(numericRange(d, xKey)[0])
       })
-      .attr('y', (d: any) => {
-        return yScale(d[yKey][1])
+      .attr('y', (d) => {
+        return yScale(numericRange(d, yKey)[1])
       })
-      .attr('width', (d: any) => {
-        return xScale(d[xKey][1]) - xScale(d[xKey][0])
+      .attr('width', (d) => {
+        const range = numericRange(d, xKey)
+        return xScale(range[1]) - xScale(range[0])
       })
-      .attr('height', (d: any) => {
-        return yScale(d[yKey][0]) - yScale(d[yKey][1])
+      .attr('height', (d) => {
+        const range = numericRange(d, yKey)
+        return yScale(range[0]) - yScale(range[1])
       })
 
     if (colorKey) {
@@ -121,22 +146,24 @@ export class ChartRange extends Chart {
 
     const colorScale = d3.scaleLinear().domain([0, 1])
     if (this.options.colorScale === AUTO_SCALE) {
-      colorScale.domain(
-        d3.extent(this.data, (d: any): number => {
-          return d[colorKey]
-        }),
-      )
+      const colorValues = this.data
+        .map((d) => d[colorKey])
+        .filter((value): value is number => typeof value === 'number')
+      const colorExtent = d3.extent(colorValues)
+      if (colorExtent[0] !== undefined && colorExtent[1] !== undefined) {
+        colorScale.domain(colorExtent)
+      }
     }
     const colorMap = this.colorMap
 
     const t = d3.transition().duration(this.options.transitionTime).ease(d3.easeLinear)
 
     const arcGenerator = d3
-      .arc()
-      .innerRadius((d) => axis.radialScale(d[rKey][0]))
-      .outerRadius((d) => axis.radialScale(d[rKey][1]))
-      .startAngle((d) => axis.angularScale(d[tKey][0]))
-      .endAngle((d) => axis.angularScale(d[tKey][1]))
+      .arc<DataPoint>()
+      .innerRadius((d) => axis.radialScale(numericRange(d, rKey)[0]))
+      .outerRadius((d) => axis.radialScale(numericRange(d, rKey)[1]))
+      .startAngle((d) => axis.angularScale(numericRange(d, tKey)[0]))
+      .endAngle((d) => axis.angularScale(numericRange(d, tKey)[1]))
 
     this.group = this.selectGroup(axis, 'chart-range')
     this.group.style('stroke', 'none')
@@ -168,14 +195,16 @@ export class ChartRange extends Chart {
       enter
         .on('pointerover', (e: any, d: any) => {
           axis.tooltip.show()
-          let x = 0
-          let y = 0
+          let x: number
+          let y: number
           if (tooltip.anchor === TooltipAnchor.Center) {
-            const start = angularPosition(d[tKey][0])
-            const end = angularPosition(d[tKey][1])
+            const tRange = numericRange(d, tKey)
+            const rRange = numericRange(d, rKey)
+            const start = angularPosition(tRange[0])
+            const end = angularPosition(tRange[1])
             const centroid = d3.arc().centroid({
-              innerRadius: axis.radialScale(d[rKey][0]),
-              outerRadius: axis.radialScale(d[rKey][1]),
+              innerRadius: axis.radialScale(rRange[0]),
+              outerRadius: axis.radialScale(rRange[1]),
               startAngle: axis.angularScale(start),
               endAngle: axis.angularScale(end),
             })
@@ -195,12 +224,10 @@ export class ChartRange extends Chart {
             x = pointer[0]
             y = pointer[1]
           }
-          axis.tooltip.update(
-            this.toolTipFormatterPolar(d),
-            tooltip.position ?? TooltipPosition.Top,
-            x,
-            y,
-          )
+          const content = this.toolTipFormatterPolar(d)
+          if (content !== undefined) {
+            axis.tooltip.update(content, tooltip.position ?? TooltipPosition.Top, x, y)
+          }
         })
         .on('pointerout', () => {
           axis.tooltip.hide()
@@ -208,39 +235,45 @@ export class ChartRange extends Chart {
     }
 
     if (colorKey) {
-      enter.style('fill', (d: any) => {
-        return colorMap(colorScale(mean(d[colorKey])))
+      enter.style('fill', (d: DataPoint) => {
+        const value = d[colorKey]
+        return typeof value === 'number' ? colorMap(colorScale(value)) : 'none'
       })
     }
 
     const update = elements.transition(t).call(arcTween, this.previousData)
 
     if (colorKey) {
-      update.style('fill', (d: any) => {
-        return colorMap(colorScale(mean(d[colorKey])))
+      update.style('fill', (d: DataPoint) => {
+        const value = d[colorKey]
+        return typeof value === 'number' ? colorMap(colorScale(value)) : 'none'
       })
     }
 
-    this.previousData = { ...this.data }
-    function arcTween(transition: any, p: any) {
-      transition.attrTween('d', (d: any, i: number, _a: any) => {
-        const old = p[i]
-        if (mean(old[tKey]) - mean(d[tKey]) > 180) {
-          old[tKey] = old[tKey].map((x) => {
-            return x - 360
-          })
-        } else if (mean(old[tKey]) - mean(d[tKey]) < -180) {
-          old[tKey] = old[tKey].map((x) => {
-            return x + 360
-          })
+    this.previousData = this.data.map((dataPoint) => ({ ...dataPoint }))
+    function arcTween(
+      transition: d3.Transition<d3.BaseType, DataPoint, SVGGElement, unknown>,
+      p: DataPoint[],
+    ) {
+      transition.attrTween('d', (d: DataPoint, i: number) => {
+        const old = p[i] ?? d
+        const oldAngles = numericRange(old, tKey)
+        const angles = numericRange(d, tKey)
+        if (mean(oldAngles) - mean(angles) > 180) {
+          oldAngles[0] = oldAngles[0] - 360
+          oldAngles[1] = oldAngles[1] - 360
+        } else if (mean(oldAngles) - mean(angles) < -180) {
+          oldAngles[0] = oldAngles[0] + 360
+          oldAngles[1] = oldAngles[1] + 360
         }
-
-        const tInterpolate = d3.interpolateArray(old[tKey], d[tKey])
-        const rInterpolate = d3.interpolateArray(old[rKey], d[rKey])
-        return (x: any) => {
+        const oldRadius = numericRange(old, rKey)
+        const radius = numericRange(d, rKey)
+        const tInterpolate = d3.interpolateArray(oldAngles, angles)
+        const rInterpolate = d3.interpolateArray(oldRadius, radius)
+        return (x: number) => {
           d[tKey] = tInterpolate(x)
           d[rKey] = rInterpolate(x)
-          return arcGenerator(d)
+          return arcGenerator(d) ?? ''
         }
       })
     }
