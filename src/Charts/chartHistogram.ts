90bc9d1c51c71fbd15a9ff5e0bfb81d449d21043
Werner Kramer <werner.kramer@deltares.nl>
2026-09-16 23:33:06 +0200

    refactor: enhance type safety and improve data handling in ChartHistogram class

--- src/Charts/chartHistogram.ts
+++ src/Charts/chartHistogram.ts
@@ -2,8 +2,9 @@ import * as d3 from 'd3'
 import { CartesianAxes, PolarAxes } from '../index.js'
 import type { CartesianAxesIndex } from '../Axes/cartesianAxes.js'
 import type { AxisIndex } from '../Axes/axes.js'
-import { Chart, AUTO_SCALE } from './chart.js'
+import { Chart, AUTO_SCALE, ChartOptions } from './chart.js'
 import { TooltipAnchor, TooltipPosition } from '../Tooltip/tooltip.js'
+import type { DataPoint } from '../Data/types.js'
 
 export class ChartHistogram extends Chart {
   plotterCartesian(axis: CartesianAxes, axisIndex: CartesianAxesIndex) {
@@ -14,9 +15,10 @@ export class ChartHistogram extends Chart {
     const xScale = axis.xScales[axisIndex.x.axisIndex]
     const yScale = axis.yScales[axisIndex.y.axisIndex]
 
-    const x1 = d3.scaleBand().domain(
-      data.map(function (d: any) {
-        return d[xKey]
+    const x1 = d3.scaleBand<string>().domain(
+      data.flatMap((d) => {
+        const value = d[xKey]
+        return typeof value === 'string' ? [value] : []
       }),
     )
     x1.range(xScale.range())
@@ -25,18 +27,20 @@ export class ChartHistogram extends Chart {
 
     const colorScale = d3.scaleLinear().domain([0, 1])
     if (this.options.colorScale === AUTO_SCALE) {
-      colorScale.domain(
-        d3.extent(this.data, function (d: any): number {
-          return d[colorKey]
-        }),
-      )
+      const colorValues = this.data
+        .map((d) => d[colorKey])
+        .filter((value): value is number => typeof value === 'number')
+      const colorExtent = d3.extent(colorValues)
+      if (colorExtent[0] !== undefined && colorExtent[1] !== undefined) {
+        colorScale.domain(colorExtent)
+      }
     }
 
     const colorMap = this.colorMap
     this.group = this.selectGroup(axis, 'chart-range')
     const t = d3.transition().duration(this.options.transitionTime).ease(d3.easeLinear)
 
-    const elements: any = this.group.selectAll('rect').data(this.data)
+    const elements = this.group.selectAll<SVGRectElement, DataPoint>('rect').data(this.data)
 
     // remove
     elements.exit().remove()
@@ -44,60 +48,62 @@ export class ChartHistogram extends Chart {
     const update = elements
       .enter()
       .append('rect')
-      .attr('y', function (d: any) {
+      .attr('y', (d) => {
         return d[yKey] === null ? yScale(0) : Math.min(yScale(d[yKey]), yScale(0))
       })
-      .attr('height', function (d: any) {
+      .attr('height', (d) => {
         return d[yKey] === null ? 0 : Math.abs(yScale(0) - yScale(d[yKey]))
       })
       .merge(elements)
-      .attr('x', function (d: any) {
-        return x1(d[xKey])
+      .attr('x', (d) => {
+        return x1(d[xKey] as unknown as string) ?? 0
       })
       .attr('width', x1.bandwidth())
 
     if (this.options.tooltip !== undefined) {
+      const tooltip = this.options.tooltip
       update
-        .on('pointerover', function (_e: any, d) {
-          if (
-            this.options.tooltip.anchor !== undefined &&
-            this.options.tooltip.anchor !== TooltipAnchor.Top
-          ) {
+        .on('pointerover', (_e: Event, d: DataPoint) => {
+          if (tooltip.anchor !== undefined && tooltip.anchor !== TooltipAnchor.Top) {
             console.error(
               'Tooltip not implemented for anchor ',
-              this.options.tooltip.anchor,
+              tooltip.anchor,
               ', using ',
               TooltipAnchor.Top,
               ' instead.',
             )
           }
           axis.tooltip.show()
-          axis.tooltip.update(
-            this.toolTipFormatterCartesian(d),
-            this.options.tooltip.position !== undefined
-              ? this.options.tooltip.position
-              : TooltipPosition.Top,
-            axis.margin.left + x1(d[xKey]) + x1.bandwidth() / 2,
-            axis.margin.top + Math.min(yScale(d[yKey]), yScale(0)),
-          )
+          const content = this.toolTipFormatterCartesian(d)
+          if (content !== undefined) {
+            const xPosition = x1(d[xKey] as unknown as string) ?? 0
+            axis.tooltip.update(
+              content,
+              tooltip.position ?? TooltipPosition.Top,
+              axis.margin.left + xPosition + x1.bandwidth() / 2,
+              axis.margin.top + Math.min(yScale(d[yKey]), yScale(0)),
+            )
+          }
         })
         .on('pointerout', () => {
           axis.tooltip.hide()
         })
     }
-    update.style('fill', function (d: any) {
-      return colorMap(colorScale(d[colorKey]))
+    update.style('fill', (d) => {
+      const value = d[colorKey]
+      return typeof value === 'number' ? colorMap(colorScale(value)) : 'none'
     })
 
     elements
       .transition(t)
-      .style('fill', function (d: any) {
-        return colorMap(colorScale(d[colorKey]))
+      .style('fill', (d) => {
+        const value = d[colorKey]
+        return typeof value === 'number' ? colorMap(colorScale(value)) : 'none'
       })
-      .attr('y', function (d: any) {
+      .attr('y', (d) => {
         return d[yKey] === null ? yScale(0) : Math.min(yScale(d[yKey]), yScale(0))
       })
-      .attr('height', function (d: any) {
+      .attr('height', (d) => {
         return d[yKey] === null ? 0 : Math.abs(yScale(0) - yScale(d[yKey]))
       })
   }
@@ -123,7 +129,7 @@ export class ChartHistogram extends Chart {
     return svg.node()
   }
 
-  setPadding(scale: any, options) {
+  setPadding(scale: d3.ScaleBand<string>, options?: ChartOptions['x']) {
     if (options?.paddingOuter) {
       scale.paddingOuter(options.paddingOuter)
     }
