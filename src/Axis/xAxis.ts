3bda5ab126e6d6f585fdf490d5c62d9fba8de443
Werner Kramer <werner.kramer@deltares.nl>
2026-09-16 22:32:21 +0200

    refactor: enhance type safety for spanScale in Axis, XAxis, and YAxis constructors

--- src/Axis/xAxis.ts
+++ src/Axis/xAxis.ts
@@ -12,7 +12,7 @@ export class XAxis extends Axis {
   constructor(
     group: d3.Selection<SVGGElement, unknown, null, unknown>,
     scale: any,
-    spanScale: any,
+    spanScale: d3.ScaleContinuousNumeric<number, number, never>,
     options: Partial<BaseAxisOptions>,
   ) {
     options.orientation ??=
