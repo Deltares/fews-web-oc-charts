ece2b5cc721fcf4c3fd98c3d6174434b1383495d
Werner Kramer <werner.kramer@deltares.nl>
2026-09-16 21:55:28 +0200

    refactor: streamline Grid class constructor and improve type definitions

--- src/Grid/grid.ts
+++ src/Grid/grid.ts
@@ -6,20 +6,19 @@ export interface GridOptions {
 }
 
 export class Grid {
-  public options: GridOptions
-  public axis: any
-  public span: any
-  private group: any
+  private group!: d3.Selection<SVGGElement, unknown, null, unknown>
 
-  constructor(group, axis: any, span: any, options: GridOptions) {
-    this.options = options
-    this.axis = axis
-    this.span = span
-    this.create(group)
+  constructor(
+    private readonly parentGroup: d3.Selection<SVGGElement, unknown, null, unknown>,
+    public readonly axis: d3.Axis<d3.AxisDomain>,
+    public readonly span: d3.ScaleContinuousNumeric<number, number, never>,
+    public readonly options: GridOptions,
+  ) {
+    this.create()
   }
 
-  protected create(group): void {
-    this.group = group.append('g').attr('class', `grid ${this.class}`)
+  protected create(): void {
+    this.group = this.parentGroup.append('g').attr('class', `grid ${this.class}`)
   }
 
   get class(): string {
@@ -30,29 +29,35 @@ export class Grid {
     const scale = this.axis.scale()
     if (this.options.axisKey === 'x') {
       const grid = d3.axisBottom(scale)
-      const size = d3.max(this.span.range() as number[])
+      const size = d3.max(this.span.range()) ?? 0
+      const tickValues = this.axis.tickValues()
       grid.tickSize(size)
       grid.ticks(5)
-      if (this.axis.tickValues() !== null) {
-        grid.tickValues(this.axis.tickValues())
+      if (tickValues !== null) {
+        grid.tickValues(tickValues)
       }
       this.updateTicks(this.group, grid)
     } else if (this.options.axisKey === 'y') {
       const grid = d3.axisRight(scale)
-      const size = d3.max(this.span.range() as number[])
+      const size = d3.max(this.span.range()) ?? 0
+      const tickValues = this.axis.tickValues()
       grid.tickSize(size)
       grid.ticks(5)
-      if (this.axis.tickValues() !== null) {
-        grid.tickValues(this.axis.tickValues())
+      if (tickValues !== null) {
+        grid.tickValues(tickValues)
       }
       this.updateTicks(this.group, grid)
     }
   }
 
-  updateTicks(selection, gridAxis: d3.Axis<d3.AxisDomain>) {
-    selection.call(gridAxis).call((g) =>
-      g.selectAll('.tick').attr('class', (d) => {
-        return d === 0 ? 'tick zero-crossing' : 'tick'
+  updateTicks(
+    selection: d3.Selection<SVGGElement, unknown, null, unknown>,
+    gridAxis: d3.Axis<d3.AxisDomain>,
+  ) {
+    selection.call(gridAxis).call((g: d3.Selection<SVGGElement, unknown, null, unknown>) =>
+      g.selectAll('.tick').attr('class', (d: unknown) => {
+        const tickValue = d as number | string | Date | null
+        return typeof tickValue === 'number' && tickValue === 0 ? 'tick zero-crossing' : 'tick'
       }),
     )
   }
